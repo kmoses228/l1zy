6ed8fe746eed03d569ffb64279beb96c498e4187
Kevin Moses <kmoses@hubspot.com>
2019-07-31 10:25:11 -0400

    Gate

--- src/components/TheGlow.js
+++ src/components/TheGlow.js
@@ -4,16 +4,17 @@ import Konva from "konva";
 
 import { Rect } from "react-konva";
 
+const NOISE_GATE = 150;
 const BUFFER_SIZE = 5;
 let buffer = new Uint8Array(BUFFER_SIZE);
 
-const rootMeanSquare = values => {
+const quadraticMean = values => {
   const squares = values.reduce((acc, val) => acc + Math.pow(val, 2), 0);
   return Math.sqrt(squares / values.length);
 };
 
 const TheGlow = ({ audioLevel }) => {
-  const gatedValue = audioLevel >= 150 ? audioLevel : 0;
+  const gatedValue = audioLevel >= NOISE_GATE ? audioLevel : 0;
   buffer = Uint8Array.from([...buffer.slice(1, BUFFER_SIZE), gatedValue]);
 
   const r = rootMeanSquare(buffer);
